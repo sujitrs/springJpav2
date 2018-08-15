09a7eb2dc084c415b25b1c8473b135306f6f48ff
maingitrepo <gitrepo@nsdl.co.in>
2018-08-15 21:58:45 +0530

    init

--- src/main/java/com/sj/spring/jpa/springJpa/SpringJpaApplication.java
+++ src/main/java/com/sj/spring/jpa/springJpa/SpringJpaApplication.java
@@ -2,8 +2,10 @@ package com.sj.spring.jpa.springJpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringJpaApplication {
 
 	public static void main(String[] args) {
